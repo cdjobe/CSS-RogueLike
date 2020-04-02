6edcc975a0c3394a32f08e730535c7fc849e5a1e
Carson Jobe <cdjobe@gmail.com>
2020-04-02 14:22:36 -0500

    rewrote how the random rooms are created. need to add doors back

--- game.js
+++ game.js
@@ -1,15 +1,61 @@
+$(document).ready(function() {
+window.addEventListener("keydown", function(e) {
+    // space and arrow keys
+	if ( [32, 37, 38, 39, 40].indexOf(e.keyCode) > -1 ) {
+        e.preventDefault();
+	}
+}, false);
+
+function randomInt(min, max) { // min and max included 
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
 class Wall {
     constructor() {
         this.type = "wall";
+        this.output = "<div class='box wall'></div>";
         this.description = "just a wall";
         this.passable = false;
     }
 };
+
 class Floor {
     constructor() {
+        this.output = "<div class='box floor'></div>";
         this.type = "floor";
         this.description = "just a floor";
         this.passable = true;
+    }
+};
+
+class Room {
+    constructor( topLeft, bottomRight ) {
+        this.topLeft = topLeft;
+        this.bottomRight = bottomRight;
+        
+        console.log(topLeft, bottomRight)
+        
+        this.createRoom = function() {
+            var height = bottomRight[0] - bottomRight[0];
+            var width = bottomRight[1] - bottomRight[1];
+                        
+            for ( var i = topLeft[0]; i < bottomRight[0]; i++ ) {              
+                // left wall
+                world[i][topLeft[1]] = new Wall;
+                // right wall
+                world[i][bottomRight[1]] = new Wall;
+                for ( var j = topLeft[1]; j <= bottomRight[1]; j++ ) {
+                    // top wall
+                    world[topLeft[0]][j] = new Wall();
+                    // bottom wall
+                    // console.log("bottomright 0  = ", bottomRight[0])
+                    world[bottomRight[0]][j] = new Wall();
+                }
+            }
+        }
+
+        this.createRoom();
+
     }
 };
 
@@ -20,41 +66,17 @@ class Player {
         this.type = "player";
         this.health = 100;
     }
-
+    
 };
 
-$(document).ready(function() {
-            // var world = [
-            //         [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-            //         [1, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-            //         [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-            //         [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-            //         [1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-            //         [1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-            //         [1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-            //         [1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-            //         [1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-            //         [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-            //         [1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-            //         [1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-            //         [1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-            //         [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-            //         [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-            //         [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-            //         [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-            //         [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
-            //     ];
-
-//***********************************************************************//
-    var worldHeight = 25;
-    var worldWidth = 25;
+    
+    var worldHeight = 30;
+    var worldWidth = 30;
     var world = [...Array(worldHeight)].map(e => Array(worldWidth).fill(new Floor));
-
     makeWorld();
-    makeRooms(0, worldHeight, worldHeight, 0, worldWidth, worldWidth, 5);
     displayWorld();
-
-//***********************************************************************//
+    
+    //***********************************************************************//
     function makeWorld() {
         for(var i = 0; i < world.length; i++) {
             // console.log(i)
@@ -65,62 +87,109 @@ $(document).ready(function() {
             world[0][i] = new Wall;
             world[world.length-1][i] = new Wall;
         }
-    }
-            
-    function randomInt(min, max) { // min and max included 
-        return Math.floor(Math.random() * (max - min + 1) + min);
-    }
+        player = new Player;
+        world[1][1] = player;
+        var minWidth = Math.floor(worldWidth/6);
+        // console.log(`min width = ${minWidth}`);
+        var maxWidth = Math.floor(worldWidth/3);
+        var minHeight = Math.floor(worldHeight/6);
+        // console.log(`min height = ${minHeight}`);
+        var maxHeight = Math.floor(worldHeight/3);
+        // top left coordinates
+        var minY = 0;
+        var minX = 0;
+        // bottom right coordinates
+        var maxY = worldHeight - 1;
+        var maxX = worldWidth -1;
 
-    function makeRooms(minY, maxY, remainY, minX, maxX, remainX, depth) {
-        var ranHeight = randomInt(remainY/4, remainY/2);
-        var ranWidth = randomInt(remainX/4, remainX/2);
+        var remainingHeight = worldHeight - minY;
+        var remainingWidth = worldWidth - minX;
+
+        makeRooms(minY, minX, maxY, maxX, 29, 29);
         
-        var remainY = remainY - ranHeight;
-        var remainX = worldWidth - ranWidth;
-
-        if (remainY >= 4 && ranHeight > 3) {
-            // console.log('**************')
-            // console.log('depth = ' + depth)
-            // console.log('minY = ' + minY)
-            // console.log('ranHeight = ' + ranHeight)
-            // console.log('ranWidth = ' + ranWidth)
-            // console.log('remainY = ' + remainY)
-
-            for (var i = minY; i < minY + ranHeight; i++) {
-                world[i][ranWidth+minX] = new Wall;
-                for (var j = 0; j < worldWidth; j++) {
-                    world[ranHeight+minY][j] = new Wall;
+        function makeRooms(minY, minX, maxY, maxX) {
+            var roomHeight = maxY - minY;
+            var roomWidth = maxX - minX;
+            console.log(`Height = ${roomHeight}, Width = ${roomWidth}`);
+            
+            if ( roomHeight < minHeight || roomWidth < minWidth ) {
+                while (roomHeight <= minHeight) {
+                    roomHeight++
                 }
-                if (remainX < (worldHeight * 0.5)) {
-                    var newWidth = remainX / 2;
-                    for (var k = minY; k < minY + ranHeight; k++) {
-                        world[k][newWidth+ranWidth] = new Wall;
-                        remainX = worldWidth - newWidth;
+                while ( roomWidth <= minWidth) {
+                    roomWidth++
+                }
+                console.log("returning nothing");
+                return new Room([minY, minX], [maxY, maxX]);
+            }
+            
+            if (    ((roomWidth <= maxWidth) && (roomWidth => minWidth))  &&
+                    ((roomHeight <= maxHeight) && (roomHeight => minHeight))
+                ){
+                    return new Room([minY, minX], [maxY, maxX]);
+            };
+        
+            if ( roomHeight > roomWidth  ) {
+                console.log( "room height > room width")
+                var randomHeight = randomInt( minHeight, maxHeight );
+                var newRoom = makeRooms(  minY, minX, 
+                            minY + randomHeight, maxX 
+                );
+                
+                var newRoomTwo = makeRooms(  minY + randomHeight, minX, 
+                            maxY, maxX
+                );
+            }
+            else if ( roomHeight  <  roomWidth ) {
+                console.log( "room height < room width")
+                var randomWidth = randomInt ( minWidth, maxWidth );
+                makeRooms(  minY, minX,   // minY, minX
+                            maxY, minX + randomWidth  // maxY, maxX
+                );
+                
+                makeRooms(  minY, minX + randomWidth, 
+                            maxY, maxX
+                );
+            }
+            else {
+                var direction = randomInt(1,2);
+                console.log(`direction = ${direction}`);
+                
+                if ( direction == 1 ) {
+
+                    var randomHeight = randomInt( minHeight, maxHeight )
+                    while ( minY + randomHeight < minHeight ) {
+                        randomHeight++;
                     }
-                    var door = randomInt(minY+1, ranHeight-1);
-                    world[door][newWidth+ranWidth] = new Floor;
+                    makeRooms(  minY, minX,
+                                minY + randomHeight, maxX
+                    );
+                    makeRooms ( minY + randomHeight, minX,
+                                maxY, maxX
+                    );
+                }
+                else {
+                    var randomWidth = randomInt ( minWidth, maxWidth )
+                    while ( minX + randomWidth < minWidth ) {
+                        randomWidth++;
+                    }
+                    makeRooms(  minY, minX,
+                                maxY, minX + randomWidth
+                    );
+                    makeRooms(  minY, minX + randomWidth ,
+                                maxY, maxX
+                    );
                 }
             }
-
-            var doorOne = randomInt(minX+1, ranWidth-1);
-            var doorTwo = randomInt(ranWidth+1, worldWidth-1);
-
-            world[ranHeight+minY][doorOne] = new Floor;
-            world[ranHeight+minY][doorTwo] = new Floor;
-
-            var door = randomInt(minY+1, minY+ranHeight-1);
-            world[door][ranWidth+minX] = new Floor;
-
-            makeRooms(minY + ranHeight, maxY, remainY, minX, maxX, worldWidth, depth-1 );
-        }              
-    }
+        }
+    }     
 
     function displayWorld() {
         var output = '';
 
         for(var i=0; i<world.length; i++) {
             output += "<div class='row'>";
-            for(var j=0; j<world[i].length; j++) {
+            for(var j=0; j<world[i].length - 1; j++) {
                 if (world[i][j].type == "player") {
                     // console.log("player at i = " + i + "j = " + j);
                     output += "<div class='box player'></div>";
@@ -136,9 +205,7 @@ $(document).ready(function() {
         }
         $('#world').html(output);
     }
-    player = new Player;
-    world[1][1] = player;
-    var obj = new Floor;
+
 
     function showDescBox(desc) {
         $('#text').text(desc);
@@ -153,6 +220,7 @@ $(document).ready(function() {
         if (e.keyCode == 40) {
             if (world[player.y+1][player.x].passable == true) {
                 obj = world[player.y+1][player.x]
+                console.log(world[player.y+1][player.x])
                 world[player.y][player.x] = obj;
                 player.y++;
                 world[player.y][player.x] = player;
@@ -178,9 +246,9 @@ $(document).ready(function() {
         // Left
         if (e.keyCode == 37) {
             if (world[player.y][player.x - 1].passable == true) {
+                console.log(obj)
                 world[player.y][player.x] = obj;
                 player.x--;
-
                 world[player.y][player.x] = player;
             }
             else {
@@ -191,6 +259,8 @@ $(document).ready(function() {
         // Right
         if (e.keyCode == 39) {
             if (world[player.y][player.x + 1].passable == true) {
+                console.log(world[player.y][player.x])
+                obj = world[player.y][player.x + 1]
                 world[player.y][player.x] = obj;
                 player.x++;
                 world[player.y][player.x] = player;
